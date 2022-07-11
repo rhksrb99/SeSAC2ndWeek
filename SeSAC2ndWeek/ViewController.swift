2ae52a4bd0ec9d7ecf5fdbdba04a5358513af8af
rhksrb99 <rhksrb99@gmail.com>
2022-07-11 12:45:20 +0900

    navigation, tap bar 연습

--- SeSAC2ndWeek/ViewController.swift
+++ SeSAC2ndWeek/ViewController.swift
@@ -9,9 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var topView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // 코너를 깎을 수 있는
+//        topView.layer.cornerRadius = 20
+//        topView.layer.shadowPath
+        // cornerRadius에 대한 적용 유무
+//        topView.clipsToBounds = true
+        // 한쪽의 코너에만 깍는 값을 주고싶을 때
+        topView.layer.maskedCorners = [.layerMinXMaxYCorner]
+        topView.layer.cornerRadius = 30
+        
     }
 
 

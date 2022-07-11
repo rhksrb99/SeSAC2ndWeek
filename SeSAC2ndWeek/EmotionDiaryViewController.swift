7f34348c1d13dfc5c2ba5ca7e3f2e0220483a2c1
rhksrb99 <rhksrb99@gmail.com>
2022-07-12 00:18:00 +0900

    감정 다이어리 액션 부분 구현중

---
+++ SeSAC2ndWeek/EmotionDiaryViewController.swift
@@ -0,0 +1,44 @@
+//
+//  EmotionDiaryViewController.swift
+//  SeSAC2ndWeek
+//
+//  Created by 박관규 on 2022/07/12.
+//
+
+import UIKit
+
+class EmotionDiaryViewController: UIViewController {
+
+    @IBOutlet weak var btn_1: UIButton!
+    @IBOutlet weak var btn_2: UIButton!
+    @IBOutlet weak var btn_3: UIButton!
+    @IBOutlet weak var btn_4: UIButton!
+    @IBOutlet weak var btn_5: UIButton!
+    @IBOutlet weak var btn_6: UIButton!
+    @IBOutlet weak var btn_7: UIButton!
+    @IBOutlet weak var btn_8: UIButton!
+    @IBOutlet weak var btn_9: UIButton!
+    
+    @IBOutlet weak var lb_1: UILabel!
+    @IBOutlet weak var lb_2: UILabel!
+    @IBOutlet weak var lb_3: UILabel!
+    @IBOutlet weak var lb_4: UILabel!
+    @IBOutlet weak var lb_5: UILabel!
+    @IBOutlet weak var lb_6: UILabel!
+    @IBOutlet weak var lb_7: UILabel!
+    @IBOutlet weak var lb_8: UILabel!
+    @IBOutlet weak var lb_9: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+    @IBAction func btn_1(_ sender:UIButton){
+//        Int(lb_1.text!)! += 1
+    }
+    
+    
+}
